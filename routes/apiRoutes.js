189b4b85c153b4c5d0ab157ae69a912b197b8cde
Raed <raed_ra@yahoo.com>
2020-05-09 10:43:05 +0800

    improve1 - fixed comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// We are linking our routes to  "data" sources.
 // ===============================================================================
 
 const fs = require("fs");
@@ -15,7 +14,6 @@ module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/notes", function(req, res) {
@@ -28,15 +26,10 @@ module.exports = function(app) {
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the   is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+
   // ---------------------------------------------------------------------------
 
   app.post("/api/notes", async function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
      try {
      let notesData = []  
       notesData = JSON.parse(fs.readFileSync(`./db/db.json`)) 
@@ -53,9 +46,6 @@ module.exports = function(app) {
 
 
   app.delete("/api/notes/:title", function(req, res) {
-    // Note the code hecre. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
     let notesData = [] 
     notesData = JSON.parse(fs.readFileSync(`./db/db.json`))
     var titletobeDeleted = req.params.title;  
@@ -69,8 +59,7 @@ module.exports = function(app) {
   });
 
   // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+  // I added this below code so you could clear out the data while working with the functionality.
   app.get("/api/clear", function(req, res) {
     // Empty out the arrays of data
     notesData=[]
