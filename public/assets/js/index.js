22dfd07d47e79ef33b7d3a7bba777041a73d972b
Raed <raed_ra@yahoo.com>
2020-05-09 10:13:23 +0800

    upgrade

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,14 @@
 var $noteTitle = $(".note-title");
 var $noteText = $(".note-textarea");
+var $noteCategory = $(".note-category");
 var $saveNoteBtn = $(".save-note");
 var $newNoteBtn = $(".new-note");
-var $noteList = $(".list-container .list-group");
+var $noteList = $(".list-container .titles");
+var $categoryList = $(".list-container .categories");//
 
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
+
 
 // A function for getting all notes from the db
 var getNotes = function() {
@@ -35,33 +38,38 @@ var deleteNote = function(id) {
 // If there is an activeNote, display it, otherwise render empty inputs
 var renderActiveNote = function() {
   $saveNoteBtn.hide();
-
-
-  if (activeNote.id!==undefined) {
+  if (activeNote.category!=undefined) {
+    //console.log("readonlyON")
     $noteTitle.attr("readonly", true);
     $noteText.attr("readonly", true);
+    $noteCategory.attr("readonly", true);
     $noteTitle.val(activeNote.title);
     $noteText.val(activeNote.text);
+    $noteCategory.val(activeNote.category)
   } else {
+    //console.log("readonlyOFF")
     $noteTitle.attr("readonly", false);
     $noteText.attr("readonly", false);
+    $noteCategory.attr("readonly", false);
     $noteTitle.val("");
     $noteText.val("");
+    $noteCategory.val("")
   }
 };
 
-// Get the note data from the inputs, save it to the db and update the view
+// Get the note data from the inputs, save it to the db and update the view -----------
 var handleNoteSave = function() {
   var newNote = {
+    category: $noteCategory.val(),
     title: $noteTitle.val(),
     text: $noteText.val()
-  };
+  }; 
 
   saveNote(newNote).then(function(data) {
     getAndRenderNotes();
     renderActiveNote();
   });
-};
+};//----------------------
 
 // Delete the clicked note
 var handleNoteDelete = function(event) {
@@ -73,9 +81,9 @@ var handleNoteDelete = function(event) {
     .data();
 
 
-  if (activeNote.id === note.id) {
+  if (activeNote.title === note.title) {
     activeNote = {};
-    deleteNote(note.id).then(function() {
+    deleteNote(note.title).then(function() {
       getAndRenderNotes();
       renderActiveNote();
     });
@@ -84,11 +92,23 @@ var handleNoteDelete = function(event) {
 
 // Sets the activeNote and displays it
 var handleNoteView =  async function() {
-  activeNote.id = $(this).data("id");
+  title = $(this).data("title");
+  $("li.list-group-item").removeClass("gray")
+  $(this).addClass("gray")
   let data= await getNotes();
-  activeNote.title = data[activeNote.id].title;
-  activeNote.text = data[activeNote.id].text;
+  activeNote = (data.filter((note)=>{
+    if (note.title === title) return note
+  }))[0]
   renderActiveNote();
+};
+
+// handles and sets up for category based view
+var handleCategoryView =  async function() {
+  cat = $(this).data("cat");    
+  let data= await getNotes();
+  $("li.list-group-item").removeClass("gray")
+  renderNoteList(data,cat);
+  $(this).addClass("gray")
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
@@ -98,48 +118,78 @@ var handleNewNoteView = function() {
 };
 
 // If a note's title or text are empty, hide the save button
-// Or else show it
+// Or else show it//--------------------
 var handleRenderSaveBtn = function() {
-  if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+  if (!$noteTitle.val().trim() || !$noteText.val().trim() || !$noteCategory.val().trim() ||activeNote.category!=undefined) {
     $saveNoteBtn.hide();
   } else {
     $saveNoteBtn.show();
   }
-};
+};//--------------------
 
-// Render's the list of note titles
-var renderNoteList = function(notes) {
+// Render's the list of note titles/-----------------
+var renderNoteList = function(notes,category) {
   $noteList.empty();
 
-  var noteListItems = [];
-
+  //var noteListItems = [];
   for (var i = 0; i < notes.length; i++) {
     var note = notes[i];
-    let objId = {"id":i}
-    var $li = $("<li class='list-group-item'>").data(objId);//
-    var $span = $("<span>").text(note.title);
-    var $delBtn = $(
-      "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
-    );
-
-    $li.append($span, $delBtn);
-    $noteList.append($li);
+    if (category === "All categories" || note.category === category) { 
+      let objTitle = {"title":note.title}
+      var $li = $("<li class='list-group-item'>").data(objTitle);//
+      var $span = $("<span>").text(note.title);
+      var $delBtn = $(
+        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      )
+      $li.append($span, $delBtn);
+      $noteList.append($li);
+    }
   }
 };
 
-// Gets notes from the db and renders them to the sidebar
+
+// Render's the list of note Categories/------------
+var renderCategoryList = function(notes) {
+  $categoryList.empty();
+
+//this is used so repetitive categories are not called more than once
+  var categories= ["All categories"]
+  cat = notes.map((note)=> {
+    if (!categories.includes(note.category)) { 
+      //categoryName = note.category;
+      categories.push(note.category);
+    }
+    return categories
+  })
+
+  for (var i = 0; i < categories.length; i++) {
+    var category = categories[i];
+    let objcat = {"cat":category}
+    var $li = $("<li class='list-group-item'>").data(objcat);//
+    var $span = $("<span>").text(category);
+    // var $delBtn = $(
+    //   "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+    // );
+    $li.append($span);
+    $categoryList.append($li);
+  }
+};//----------------
+
+// Gets notes from the db and renders them to the sidebar------------------
 var getAndRenderNotes = function() {
   return getNotes().then(function(data) {
-    renderNoteList(data);
+    renderNoteList(data,"All categories");
+    renderCategoryList(data);
   });
-};
+};//-----------------
 
 $saveNoteBtn.on("click", handleNoteSave);
 $noteList.on("click", ".list-group-item", handleNoteView);
 $newNoteBtn.on("click", handleNewNoteView);
 $noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
-
+$noteTitle.on("keyup", handleRenderSaveBtn); //
+$noteText.on("keyup", handleRenderSaveBtn); //
+$noteCategory.on("keyup", handleRenderSaveBtn);  //
+$categoryList.on("click",".list-group-item", handleCategoryView);
 // Gets and renders the initial list of notes
 getAndRenderNotes();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -52,16 +52,19 @@ module.exports = function(app) {
     //--------------
 
 
-  app.delete("/api/notes/:id", function(req, res) {
+  app.delete("/api/notes/:title", function(req, res) {
     // Note the code hecre. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body parsing middleware
     let notesData = [] 
     notesData = JSON.parse(fs.readFileSync(`./db/db.json`))
-    var idtobeDeleted = req.params.id;  
-    //delete notesData[idtobeDeleted];
-    notesData.splice(idtobeDeleted,1)
-    fs.writeFileSync(`./db/db.json`,JSON.stringify(notesData))
+    var titletobeDeleted = req.params.title;  
+     notesPostDelete = notesData.filter((note)=>{
+      if (note.title != titletobeDeleted) {
+        return note
+      } 
+    })
+    fs.writeFileSync(`./db/db.json`,JSON.stringify(notesPostDelete))
     res.json(true);
   });
 
